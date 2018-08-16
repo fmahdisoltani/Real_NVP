6dedafe9c16ce1572f5e91ca5ef7b534e5a83f8c
farzaneh <fmahdisoltani@gmail.com>
2018-08-16 12:59:40 -0400

    Add unpack_dataset func

--- discriminator/python_lib/dataset/mnist.py
+++ discriminator/python_lib/dataset/mnist.py
@@ -10,21 +10,26 @@ class MNIST(object):
     """MNIST dataset class."""
 
     @staticmethod
-    def load_data():
+    def load_as_dataset():
         mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-        data = nn.Dataset()
-        data.train.x = mnist.train._images
-        data.train.t = mnist.train._labels
-        data.train.labels = np.argmax(mnist.train._labels, axis=1)
+        dataset = nn.Dataset()
+        dataset.train.x = mnist.train._images
+        dataset.train.t = mnist.train._labels
+        dataset.train.labels = np.argmax(mnist.train._labels, axis=1)
 
-        data.eval.x = mnist.validation._images
-        data.eval.t = mnist.validation._labels
-        data.eval.labels = np.argmax(mnist.validation._labels, axis=1)
+        dataset.eval.x = mnist.validation._images
+        dataset.eval.t = mnist.validation._labels
+        dataset.eval.labels = np.argmax(mnist.validation._labels, axis=1)
 
-        data.test.x = mnist.test._images
-        data.test.t = mnist.test._labels
-        data.test.labels = np.argmax(mnist.test._labels, axis=1)
+        dataset.test.x = mnist.test._images
+        dataset.test.t = mnist.test._labels
+        dataset.test.labels = np.argmax(mnist.test._labels, axis=1)
 
-        print ("&&&&&&&&&   x.min=", data.train.x.min(), "        x.max=", data.train.x.max())
-        return data
+        print ("&&&&&&&&&   x.min=", dataset.train.x.min(), " x.max=", dataset.train.x.max())
+        return dataset
+
+    def unpack_dataset(dataset):
+        return dataset.train.x, dataset.train.t, dataset.train.labels, \
+               dataset.eval.x, dataset.eval.t, dataset.eval.labels, \
+               dataset.test.x, dataset.test.t, dataset.test.labels
